74c5043fe71eac8bb42823a39816a36957e811a2
Russell Gordon <github@russellgordon.ca>
2019-12-03 13:44:05 -0500

    Wrote code to collect and check input values.

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -27,6 +27,29 @@ class ViewController: UIViewController {
     // MARK: Actions
     @IBAction func checkStudentAnswers(_ sender: Any) {
         
+        // Clear prior output messages
+        outputResults.text = ""
+        
+        // INPUT
+        // 1. Get the number of questions on the test.
+        //    Ensure the input provided is an integer.
+        guard let questionCountAsString = inputQuestionCount.text, let questionCount = Int(questionCountAsString), questionCount > 0 else {
+            outputResults.text = "Please enter an integer value greater than 0."
+            return
+        }
+        
+        // 2. Get the student answers
+        guard let studentAnswers = inputStudentAnswers.text, studentAnswers.count == questionCount else {
+            outputResults.text = "Please be sure you input exactly \(questionCount) student answers."
+            return
+        }
+        
+        // 3. Get the correct answers
+        guard let correctAnswers = inputCorrectAnswers.text, correctAnswers.count == questionCount else {
+            outputResults.text = "Please be sure you input exactly \(questionCount) answers for the answer key."
+            return
+        }
+        
         
     }
     
